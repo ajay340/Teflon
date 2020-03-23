ec1b53115519002ed344e4bf0ba0a09a66738e45
Joshua Schappel <jmschappel12@gmail.com>
2020-03-22 21:00:19 -0400

    Lexemes work done on lexer

--- src/main.rs
+++ src/main.rs
@@ -1,8 +1,16 @@
 pub mod vm;
 pub mod instructions;
 pub mod repl;
+pub mod assembler;
+pub mod lexer;
 
 fn main() {
-    let mut repl = repl::REPL::new();
-    repl.run();
+    //let mut repl = repl::REPL::new();
+    //repl.run();
+
+
+    let mut l = lexer::Lexer::new("ADD $1 $2 $3");
+    let v = l.lex();
+
+    println!("{:?}", v);
 }
