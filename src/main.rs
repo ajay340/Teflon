5c29f4d0f84d85ead4df7ad4ca302d2f6fe86bd0
Ajay Shah <ajay@shah>
2020-05-05 01:16:45 -0400

    added structure for parsing

--- src/main.rs
+++ src/main.rs
@@ -8,9 +8,10 @@ pub mod parser;
 fn main(){
     let mut lex = lexer::Lexer::new();
     lex.lex_line("ADD #4 $3", 1);
-    let ast = parser::parser::AbstractSyntaxTree::new(lex.tokens);
-    &ast.parse();
-    //print!("{:?}", ast);
+    let mut ast = parser::parser::Parser::new(lex.tokens);
+    // ast.next();
+    // ast.next();
+    // println!("{:?}", ast.current);
 
     // let mut repl = repl::REPL::new();
     // repl.run();
--- src/parser/parser.rs
+++ src/parser/parser.rs
@@ -1,47 +1,32 @@
 use crate::lexer;
-use std::fmt;
 
-#[derive(Debug)]
-pub struct AbstractSyntaxTree {
+pub struct Parser {
     tokens: Vec<lexer::token::Token>,
-    tree:   Vec<Binary>,
-    index: usize,
+    pub current: usize
 }
 
-#[derive(Debug)]
-struct Binary {
-    operator: lexer::token::TokenType,
-    left: lexer::token::TokenType,
-    right: lexer::token::TokenType,
-}
-
-
-impl Binary{
-    fn new(op: lexer::token::TokenType, left: lexer::token::TokenType, right:lexer::token::TokenType) -> Binary{
-        Binary{operator:op, left:left, right:right}
-    }
-}
-
-impl AbstractSyntaxTree {
-    pub fn new(lex_tokens: Vec<lexer::token::Token>) -> AbstractSyntaxTree {
-        AbstractSyntaxTree {
-            tokens: lex_tokens,
-            index: 0,
-            tree: vec![],
+impl Parser {
+    pub fn new (tokens: Vec<lexer::token::Token>) -> Parser{
+        return Parser {
+            tokens: tokens, 
+            current: 0
         }
     }
-    pub fn parse(mut self){
-        for token in self.tokens{
-            match &token.token{
-                lexer::token::TokenType::EOF => println!("END OF FILE"),
-                lexer::token::TokenType::IntOperand => println!("INTEGER INCOMING"),
-                lexer::token::TokenType::NUMBER(NUMBER) => println!("NUMBER OP"),
-                lexer::token::TokenType::OPCODE(ADD) => 
-                    self.tree.push(Binary::new(token.token,lexer::token::TokenType::EOF, lexer::token::TokenType::EOF)),
-                lexer::token::TokenType::REGISTER => println!("REGISTER INCOMING"),
-                _ => println!("{:?}",token.token)
-            }
+    fn isAtEnd(self) -> bool {      
+        return match self.peek(){
+            lexer::token::TokenType::EOF => true,
+            _ => false,
         }
     }
+    fn peek(&self) -> &lexer::token::TokenType{
+        return &self.tokens.get(self.current).unwrap().token; 
+    }
+    fn previous(&self) -> &lexer::token::TokenType{
+        return &self.tokens.get(self.current - 1).unwrap().token; 
+    }
+    fn next(&mut self){
+       self.current += 1;
+    }
 }
+
 // Instruction ::= <opcode> ‘$’ <register>  ‘$’<register>  ‘$’ <register>  | <opcode> ‘$’ <register> ‘#’ <int operand>  | <opcode> ‘#’ <int operand>
